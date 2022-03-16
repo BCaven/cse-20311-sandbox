e52c027c7093919538b27e5f87895411b559c54e
John Caven <jcaven@student13.cse.nd.edu>
2022-03-16 14:17:38 -0400

    update

---
+++ lab7/path.c
@@ -0,0 +1,30 @@
+/*
+    @author: John Blake Caven
+    @name: path.c
+    @date: 3-16-22
+
+    Lab 7: Part 1
+
+*/
+
+/*
+structs
+
+
+
+*/
+
+
+
+#include <stdio.h>
+
+int main(void) {
+    struct point {
+        int x;
+        int y;
+    }
+    
+
+
+    return 0;
+}
