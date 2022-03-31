486ef3d97a0b365747242f8e936c2f16f8b3611b
John Caven <jcaven@student10.cse.nd.edu>
2022-03-31 18:02:42 -0400

    wow Im an idiot

--- lab8/crossword2.c
+++ lab8/crossword2.c
@@ -1,3 +1,5 @@
+// no, this scares me
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -6,20 +8,22 @@ typedef struct {
     char *word;
     int len;
 } word;
+
 void initBoard(char board[15][15], int type);
-void printWords(char words[20][15], int len);
 void printBoard(char board[15][15]);
-int readFile(FILE *f, char board[15][15]);
-int interactiveInput(char board[15][15]);
-void toUpperCase(char word[20], int len);
-void sortWords(char words[20][15], int numWords);
+void printWords(word words[20], int len);
+int readFile(FILE *f, word words[20]);
+int interactiveInput(word words[20]);
+void toUpperCase(word w[20], int len);
+
+
 int main(void) {
     char fName[30];
     printf("Get the file name:\n");
     scanf("%s", &fName);
     
     // make and initialize the arrays
-    char words[20][15];
+    word words[20];
     char dotBoard[15][15];
     char hashBoard[15][15];
     initBoard(dotBoard, 0);
@@ -36,16 +40,15 @@ int main(void) {
     printWords(words, numWords);
 
     int i;
-    for (i = 0; i < numWords; i+=1) {
-        toUpperCase(words[i], strlen(words[i]));
-    }
+    toUpperCase(words, numWords);
+    
     printf("Uppercase list:\n");
     printWords(words, numWords);
     printf("Sorted:\n");
     sortWords(words, numWords);
     printWords(words, numWords);
     printf("\n\n-----------\n\n");
-    char fileWords[20][15];
+    word fileWords[20];
     FILE *f;
     printf("bang\n");
     if (fopen(fName, "r") == NULL) {
@@ -57,16 +60,17 @@ int main(void) {
     int fLen = readFile(f, fileWords);
     printf("List from file:\n");
     printWords(fileWords, fLen);
+    printf("List from file, capitalized and sorted:\n");
+    toUpperCase(fileWords, fLen);
+    sortWords(fileWords, fLen);
+    printWords(fileWords, fLen);
     fclose(f);
     
     return 0;
 }
-void printWords(char words[20][15], int len) {
-    int i;
-    for (i = 0; i < len; i+=1) {
-        printf("%s\n", words[i]);
-    }
-}
+
+
+/* ------------------------ Board functions -------------------------- */
 void initBoard(char board[15][15], int type) {
     int i, j;
     char block = '#';
@@ -89,40 +93,76 @@ void printBoard(char board[15][15]) {
         printf("\n");
     }
 }
-int readFile(FILE *f, char board[20][15]) {
+/* ----------------------------- Word Functions  ----------------------------- */
+void printWords (word words[20], int len) {
+    int i;
+    for (i = 0; i < len; i+=1) {
+        printf("%s\n", words[i].word);
+    }
+}
+void toUpperCase(word w[20], int len) { // hooray, an easy function
+    int i, j;
+    for (j = 0; j < len; j+=1) {
+    for (i = 0; i < w[j].len; i+=1) {
+        w[j].word[i] = toupper(w[j].word[i]);
+    }
+    }
+    
+}
+void cleanTxt(word w) { // I dont know if I will need this, but I made it just in case
+    int i;
+    for (i = 0; i < 20; i+=1) {
+        w.word[i] = '\0';
+    }
+}
+void sortWords(word words[20], int numWords) {
+    int i, j;
+    for (i = 0; i < numWords-1; i+=1) {
+        for (j = i+1; j < numWords; j+=1) {
+            //printf("Strcmp: %d", strcmp(words[i], words[j]));
+            int c = strcmp(words[i].word, words[j].word);
+            if (c > 0) {
+                char temp[15];
+                strcpy(temp, words[i].word);
+                //cleanTxt(words[i]);
+                strcpy(words[i].word, words[j].word);
+                //cleanTxt(words[j]);
+                strcpy(words[j].word, temp);
+
+
+            }
+        }
+    }
+
+}
+/* ----------------------------- Input Functions ----------------------------- */
+int readFile(FILE *f, word words[20]) {
     int numWords = 0;
     char  currentWord[15];
     fgets(currentWord, 15, f);
     while (currentWord != NULL) {
-        // remove the newline character
-        char *clean = strtok(currentWord, "\n");
-        printf("clean: %s\n", clean);
-        strcpy(currentWord, clean);
         int lenWord = strlen(currentWord);
         // get all the words
-
         printf("strcmp %s : '.' = %d\n", currentWord, strcmp(currentWord, ".")); // for some reason it is missing the last two words
-        
         if (feof(f) || strcmp(currentWord, ".") == 0) {
-            return numWords;
+            return lenWord;
         }
-        
+        currentWord[lenWord-1] = '\0';
+        //currentWord[lenWord-2] = '\0';
         //printf("word: %s\n", currentWord);
         if (strlen(currentWord) < 15) {
-            strcpy(board[numWords], currentWord);
-            printf("copying %s to place %d\n", currentWord, numWords);
-            numWords +=1;
+
+            words[numWords].word = &currentWord;
+            words[numWords].len = strlen(currentWord);
         } else {
             printf("%s is too long\n", currentWord);
-        } 
-
-
-
+        }
+        numWords +=1;
         fgets(currentWord, 30, f);
     }
     return numWords;
 }
-int interactiveInput(char board[20][15]) {
+int interactiveInput(word words[20]) {
     int c = 0;
     char currentWord[15];
     scanf("%s", &currentWord);
@@ -131,12 +171,11 @@ int interactiveInput(char board[20][15]) {
             //printf("hit . \n");
             return c;
         }
-        //printf("current word: %s, length: %d\n", currentWord, strlen(currentWord));
-        // board[c] = currentWord;// this should work right? Or should I use strcpy
         if (strlen(currentWord) > 14) {
             printf("The word is too long\n");
         } else {
-            strcpy(board[c], currentWord);
+            words[c].word = &currentWord;
+            words[c].len = strlen(currentWord);
             c+=1;
         }
         
@@ -145,36 +184,4 @@ int interactiveInput(char board[20][15]) {
     }
     return c;
 }
-void toUpperCase(char word[20], int len) { // hooray, an easy function
-    int i;
-    for (i = 0; i < len; i+=1) {
-        word[i] = toupper(word[i]);
-    }
-    
-}
-void cleanTxt(char word[20]) { // I dont know if I will need this, but I made it just in case
-    int i;
-    for (i = 0; i < 20; i+=1) {
-        word[i] = '\0';
-    }
-}
-void sortWords(char words[20][15], int numWords) {
-    int i, j;
-    for (i = 0; i < numWords-1; i+=1) {
-        for (j = i+1; j < numWords; j+=1) {
-            //printf("Strcmp: %d", strcmp(words[i], words[j]));
-            int c = strcmp(words[i], words[j]);
-            if (c > 0) {
-                char temp[15];
-                strcpy(temp, words[i]);
-                //cleanTxt(words[i]);
-                strcpy(words[i], words[j]);
-                //cleanTxt(words[j]);
-                strcpy(words[j], temp);
-
-
-            }
-        }
-    }
-
-}
+/* ---------------------------- misc functions ---------------------------- */
--- misc/pointers.c
+++ misc/pointers.c
@@ -7,8 +7,12 @@ typedef struct {
 } word;
 word returnStruct(char *s, int len);
 void pointToStruct(word *w, char *c, int l);
+void walk(int *intArr, int len);
+int* returningPointer(int len);
 
-int main(void) {
+
+
+int main(int argc, char *argv) {
     word test;
     test.str = "hello world";
     test.len = strlen("hello world");
@@ -25,8 +29,67 @@ int main(void) {
     printf("%s\n", testString);
 
     printf("%d\n", strlen(testString));
-    
+    printf("%c\n", testString[0]);
 
+    int intPointer[10];
+    int i;
+    for (i = 0; i < 10; i+=1) {
+        intPointer[i] = i;
+    }
+    int *pointer = &intPointer;
+
+    for (i = 0; i < 10; i+=1) {
+        printf("%d ", (*(pointer+i)));
+    }
+    printf("\n");
+
+    
+    for (i = 0; i < 10; i+=1) {
+        printf("%d ", *pointer);
+        pointer++;
+    }
+    
+    printf("\n");
+    printf("%d\n", pointer);
+    
+    pointer = intPointer;
+    printf("%d\n", *pointer);
+    while (pointer < intPointer+10) {
+        printf("%d ", *pointer);
+        pointer++;
+    }
+    printf("\n");
+
+    printf("-------\n");
+    walk(&intPointer, 10);
+    printf("-------\n");
+    int *arrPointer = returningPointer(10);
+    for (i = 0; i < 10; i+=1) {
+        printf("%d ", *arrPointer);
+        arrPointer++;
+    }
+    printf("\n");
+
+    printf("\n\nargc/argv testing\n\n");
+    printf("argc: %d\n", argc);
+    char * p = argv;
+    printf("printing as string:\n");
+    printf("%s\n", argv);
+    printf("printing as chars:\n");
+    for (i = 0; i < argc; i+=1) {
+        printf("%c\n", argv[0]);
+        argv++;
+    }
+
+    printf("\n\nMalloc Testing:\n\n");
+
+
+    word *mallocTest = (word*) malloc(sizeof(word));
+    mallocTest->str = "hello random point in memory";
+    mallocTest->len = sizeof("hello random point in memory");
+    printf("%s | len: %d\n", mallocTest->str, mallocTest->len);
+
+    free(mallocTest);
 
 
     return 0;
@@ -41,4 +104,20 @@ word returnStruct(char *s, int len) {
 void pointToStruct(word *w, char *c, int l) {
     w->str = c;
     w->len = l;
+}
+void walk(int *intArr, int len) {
+    int *p = intArr;
+    while (p < intArr + len) {
+        printf("%d ", *p);
+        p++;
+    }
+    printf("\n");
+}
+int* returningPointer(int len) {
+    int a[len];
+    int i;
+    for (i = 0; i < len; i+=1) {
+        a[i] = i;
+    }
+    return &a;
 }
