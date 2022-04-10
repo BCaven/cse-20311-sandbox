5579556f8259e9e63439a36417f7833bf8dd82eb
John Caven <jcaven@student10.cse.nd.edu>
2022-04-10 17:33:19 -0400

    pointers and fun

--- lab9/graphics_are_dumb.c
+++ lab9/graphics_are_dumb.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-#include "gfx.h"
 
-typedef struct {
-    int data;
-    node *next;
-} node;
 
 int blakeIsDumb(int a, int b);
 
@@ -14,6 +9,8 @@ int main(void) {
     printf("%d * %d = %d\n", 5, 4, dumber(5, 4));
 
     // other stuff
+    int *i = (int *) malloc(sizeof(int));
+    pointerWalk(i, 100);
 
 
 
@@ -27,7 +24,18 @@ int main(void) {
 
 
 // funny
-
+void pointerWalk(int *p, int n) {
+    if (n > 1) {
+        //printf("address: %d contents as char: %c contents as int: %d\n", p, *p, *p);
+        if (*p != 0) {
+            printf("address: %d contents as char: %c contents as int: %d\n", p, *p, *p);
+            return pointerWalk(p+1, n-1);
+        } else {
+            return pointerWalk(p+1, n);
+        }
+    }
+    return;
+}
 
 
 
