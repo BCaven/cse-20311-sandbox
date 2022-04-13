34f00703e526ffe84cd4a8bca68d010b40730124
John Caven <jcaven@student10.cse.nd.edu>
2022-04-13 13:28:55 -0400

    tree

--- lab10/fractals.c
+++ lab10/fractals.c
@@ -2,13 +2,14 @@
 #include <math.h>
 #include "gfx.h"
 #define PI 3.1415926535
+void sierpinski(int x1, int y1, int x2, int y2, int x3, int y3, int n);
+void triangle(int x1, int y1, int x2, int y2, int x3, int y3);
 void shrinkingSquares(int x, int y, int size, int n);
 void square(int x, int y, int size);
 void spiralSquares(int x, int y, int size, double n);
 void circularLace(int x, int y, int size, int petals, int n);
 void snowFlake(int x, int y, int oldX, int oldY, int size, int petals, int n);
-void sierpinski(int x1, int y1, int x2, int y2, int x3, int y3, int n);
-void triangle(int x1, int y1, int x2, int y2, int x3, int y3);
+void treeFractal(int x, int y, int branches, int size, double theta, int n);
 int main(void) {
     int width = 1000;
     int height = 1000;
@@ -18,7 +19,7 @@ int main(void) {
     while (1) {
         gfx_clear();
         if (c == '1') {
-
+            sierpinski(width/10, height/10, (.9)*width, height/10, width/2, height * (.9), 7);
         } else if (c == '2') {
             shrinkingSquares(width/2, height/2, 500, 6);
         } else if (c == '3') {
@@ -28,7 +29,7 @@ int main(void) {
         } else if (c == '5') {
             snowFlake(width/2, height/2, width/2, height/2, 300, 5, 6);
         } else if (c == '6') {
-
+            treeFractal(width/2, height * (.9), 2, 200, 0, 10);
         } else if (c == '7') {
 
         } else if (c == 'q') {
@@ -40,7 +41,9 @@ int main(void) {
     }
 }
 void triangle(int x1, int y1, int x2, int y2, int x3, int y3) {
-
+    gfx_line(x1, y1, x2, y2);
+    gfx_line(x2, y2, x3, y3);
+    gfx_line(x3, y3, x1, y1);
 }
 void sierpinski(int x1, int y1, int x2, int y2, int x3, int y3, int n) {
     if( n < 0 ) {
@@ -50,9 +53,9 @@ void sierpinski(int x1, int y1, int x2, int y2, int x3, int y3, int n) {
     // Draw the triangle
     triangle(x1, y1, x2, y2, x3, y3);
     // Recursive calls
-    sierpinskiClass(x1, y1, (x1+x2)/2, (y1+y2)/2, (x1+x3)/2, (y1+y3)/2, n-1);
-    sierpinskiClass((x1+x2)/2, (y1+y2)/2, x2, y2, (x2+x3)/2, (y2+y3)/2, n-1);
-    sierpinskiClass((x1+x3)/2, (y1+y3)/2, (x2+x3)/2, (y2+y3)/2, x3, y3, n-1);
+    sierpinski(x1, y1, (x1+x2)/2, (y1+y2)/2, (x1+x3)/2, (y1+y3)/2, n-1);
+    sierpinski((x1+x2)/2, (y1+y2)/2, x2, y2, (x2+x3)/2, (y2+y3)/2, n-1);
+    sierpinski((x1+x3)/2, (y1+y3)/2, (x2+x3)/2, (y2+y3)/2, x3, y3, n-1);
     
 }
 void square(int x, int y, int size) {
@@ -101,5 +104,17 @@ void snowFlake(int x, int y, int oldX, int oldY, int size, int petals, int n) {
         int newX = x + (sin(i)*size);
         int newY = y + (cos(i)*size);
         snowFlake(newX, newY, x, y, (1.5*size)/petals, petals, n-1);
+    }
+}
+void treeFractal(int x, int y, int branches, int size, double theta, int n) {
+    if (n < 0) {
+        return;
+    }
+    int newX = x + (size*sin(theta));
+    int newY = y - (size*cos(theta));
+    gfx_line(x, y, newX, newY);
+    double i;
+    for (i = -PI/4; i <= PI/4; i += PI/branches) {
+        treeFractal(newX, newY, branches, size * (.75), theta + i, n-1);
     }
 }
