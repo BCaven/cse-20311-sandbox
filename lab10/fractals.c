59c2510587cc0497d93800e845f3578239e8b8df
John Caven <jcaven@student10.cse.nd.edu>
2022-04-13 16:53:40 -0400

    fern done

--- lab10/fractals.c
+++ lab10/fractals.c
@@ -10,9 +10,11 @@ void spiralSquares(int x, int y, int size, double n);
 void circularLace(int x, int y, int size, int petals, int n);
 void snowFlake(int x, int y, int oldX, int oldY, int size, int petals, int n);
 void treeFractal(int x, int y, int branches, int size, double theta, int n);
+void fernFractal(int x, int y, int branches, int size, double theta, int n, int tn);
+void spiralFractal(int x, int y, int size, double n, double max);
 int main(void) {
-    int width = 1000;
-    int height = 1000;
+    int width = 500;
+    int height = 500;
     char *name = "Fractals";
     gfx_open(width, height, name);
     char c = '0';
@@ -21,17 +23,20 @@ int main(void) {
         if (c == '1') {
             sierpinski(width/10, height/10, (.9)*width, height/10, width/2, height * (.9), 7);
         } else if (c == '2') {
-            shrinkingSquares(width/2, height/2, 500, 6);
+            shrinkingSquares(width/2, height/2, width/2, 6);
         } else if (c == '3') {
             spiralSquares(width/2, height/2, 1, 0);
         } else if (c == '4') {
-            circularLace(width/2, height/2, 200, 6, 6);
+            circularLace(width/2, height/2, width/4, 6, 6);
         } else if (c == '5') {
-            snowFlake(width/2, height/2, width/2, height/2, 300, 5, 6);
+            snowFlake(width/2, height/2, width/2, height/2, width/4, 5, 6);
         } else if (c == '6') {
-            treeFractal(width/2, height * (.9), 2, 200, 0, 10);
+            treeFractal(width/2, height * (.9), 2, width/6, 0, 10);
         } else if (c == '7') {
-
+            int n = 2;
+            fernFractal(width/2, height * (.9), 2, width/6, 0, n, n);
+        } else if (c == '8') {
+            spiralFractal(width/2, height/2, 10, 0, 2*PI);
         } else if (c == 'q') {
             return 0;
         } else {
@@ -50,9 +55,9 @@ void sierpinski(int x1, int y1, int x2, int y2, int x3, int y3, int n) {
         return;
     }
 
-    // Draw the triangle
+    // draw the current
     triangle(x1, y1, x2, y2, x3, y3);
-    // Recursive calls
+    // draw the inner triangles
     sierpinski(x1, y1, (x1+x2)/2, (y1+y2)/2, (x1+x3)/2, (y1+y3)/2, n-1);
     sierpinski((x1+x2)/2, (y1+y2)/2, x2, y2, (x2+x3)/2, (y2+y3)/2, n-1);
     sierpinski((x1+x3)/2, (y1+y3)/2, (x2+x3)/2, (y2+y3)/2, x3, y3, n-1);
@@ -79,8 +84,7 @@ void spiralSquares(int x, int y, int size, double n) {
     if (n > 40) {
         return;
     }
-    spiralSquares(x + (cos(n) * (size*2)), y + (sin(n) * (size*2)), size + (n/4), n+.5);
-    
+    spiralSquares(x + (cos(n) * (size*2)), y + (sin(n) * (size*2)), size + (n/4), n+.5); // Mmmmmmmm
 }
 void circularLace(int x, int y, int size, int petals, int n) {
     if (n < 0) {
@@ -89,7 +93,7 @@ void circularLace(int x, int y, int size, int petals, int n) {
     gfx_circle(x, y, size);
     double i;
     for (i = 0; i < 2*PI; i+= (2*PI)/petals) {
-        int newX = x + (cos(i)*size);
+        int newX = x + (cos(i)*size); // split these up so they were easier to edit
         int newY = y + (sin(i)*size);
         circularLace(newX, newY, (2*size)/petals, petals, n-1);
     }
@@ -101,7 +105,7 @@ void snowFlake(int x, int y, int oldX, int oldY, int size, int petals, int n) {
     gfx_line(x, y, oldX, oldY);
     double i;
     for (i = 0; i < 2*PI; i+= (2*PI)/petals) {
-        int newX = x + (sin(i)*size);
+        int newX = x + (sin(i)*size); // this was totally not copied from circularLace()
         int newY = y + (cos(i)*size);
         snowFlake(newX, newY, x, y, (1.5*size)/petals, petals, n-1);
     }
@@ -110,11 +114,42 @@ void treeFractal(int x, int y, int branches, int size, double theta, int n) {
     if (n < 0) {
         return;
     }
-    int newX = x + (size*sin(theta));
+    int newX = x + (size*sin(theta)); // once again split out to be easier to edit
     int newY = y - (size*cos(theta));
     gfx_line(x, y, newX, newY);
     double i;
     for (i = -PI/4; i <= PI/4; i += PI/branches) {
         treeFractal(newX, newY, branches, size * (.75), theta + i, n-1);
     }
+}
+void fernFractal(int x, int y, int branches, int size, double theta, int n, int tn) {
+    if (size < 1) {
+        return;
+    }
+    
+    if (n < 0) {
+        return;
+    }
+    
+    int newX = x + (size*sin(theta));
+    int newY = y - (size*cos(theta));
+    gfx_line(x, y, newX, newY);
+    double i;
+    for (i = -PI/4.0; i <= PI/4.0; i+= PI/4.0) {
+        if (i == 0) {
+            fernFractal(newX, newY, branches, size, theta, n-1, tn);
+        } else {
+            fernFractal(newX, newY, branches, size/3, theta + i, tn, tn);
+        }
+    }
+
+}
+void spiralFractal(int x, int y, int size, double n, double max) {
+    square(x, y, 1);
+    if (n > max) {
+        return;
+    }
+    double c = .1;
+    spiralFractal(x + (cos(n) * (c)), y + (sin(n) * (c)), size, n+.5, max);
+    spiralFractal(x + (cos(n) * (size*2)), y + (sin(n) * (size*2)), size + (n/4), n+.5, max); // Mmmmmmmm
 }
