41aa5a31dd814dc54a3c1706588eadecbb734748
John Caven <jcaven@student13.cse.nd.edu>
2022-04-26 16:34:28 -0400

    paint is coming along

--- lab11/modified_paint/paint_v2.c
+++ lab11/modified_paint/paint_v2.c
@@ -8,6 +8,7 @@ typedef struct {
     int b;
     int t;
 } color;
+color Color(int r, int g, int b, int t);
 void paintScreen(char paint[], int l, int width, color colors[]);
 void drawDot(int x, int y, int brush, int currentColor, char canvas[], int width);
 double distance(int x1, int y1, int x2, int y2);
@@ -26,20 +27,20 @@ int main(void) {
     }
     int mode = 0;
     color currentColor;
-    currentColor = Color(0, 0, 0);
+    currentColor = Color(0, 0, 0, 1);
     while (1) {
-        gfx_color(100, 100, 100);
-        gfx_text(10, 10, "paint program");
         
         if (gfx_event_waiting()) {
             char c = gfx_wait();
             if (mode == 0) { // view
-                if (c == '1') {
-
-                } else if (c == '2') {
-
-                } else if (c == '3') {
-
+                if (c == '1') { // dots
+                    mode = 1;
+                } else if (c == '2') { // lines
+                    mode = 2;
+                } else if (c == '3') { // save
+                    mode = 3;
+                } else if (c == 'q') {
+                    return 0;
                 }
             } else if (mode == 1) { // draw dots
                 if (c == 1) { // mouse press
@@ -47,21 +48,39 @@ int main(void) {
                 } else if (c >= '0' && c <= '9') { // all characters 1-9
                     int col = c - '0';
 
+                } else if (c == 'q') {
+                    mode = 0;
                 }
             } else if (mode == 2) { // draw lines
+                if (c == 1) { // mouse press
 
+                } else if (c >= '0' && c <= '9') { // all characters 1-9
+                    int col = c - '0';
+
+                } else if (c == 'q') {
+                    mode = 0;
+                }
             } else if (mode == 3) { // erase
 
             }
+            gfx_clear();
         }
         // what to print for each mode:
         if (mode == 0) {
-
+            gfx_color(100, 100, 100);
+            gfx_text(10, 10, "paint program - select a mode by pressing [1] [2] or [3]");
+        
         } else if (mode == 1) {
+            gfx_color(100, 100, 100);
+            gfx_text(10, 10, "paint program - dot drawing mode - select a color by pressing 1-9, press the mouse to draw");
 
         } else if (mode == 2) {
+            gfx_color(100, 100, 100);
+            gfx_text(10, 10, "paint program - line drawing mode - select a color by pressing 1-9, press the mouse to draw");
 
         } else if (mode == 3) {
+            gfx_color(100, 100, 100);
+            gfx_text(10, 10, "paint program - file mode - not implemented yet");
 
         }
     }
@@ -73,7 +92,12 @@ int main(void) {
 
 // constructors
 color Color(int r, int g, int b, int t) {
-    
+    color c;
+    c.r = r;
+    c.g = g;
+    c.b = b;
+    c.t = t;
+    return c;
 }
 
 // displaying the screen
@@ -119,8 +143,8 @@ void drawLine(int x1, int y1, int x2, int y2, int currentColor, int brush, char 
     nX = x1;
     nY = y1;
     double d = distance(x1, y1, x2, y2);
-    double stepx = (x2 - x1) / distance;
-    double stepy = (y2 - y1) / distance;
+    double stepx = (x2 - x1 * 1) / d;
+    double stepy = (y2 - y1 * 1) / d;
     for (i = (double) (x1); i < x2; i += stepx) {
         for (j = (double) (y1); j < y2; j += stepy) {
             drawDot((int) (i), (int) (j), brush, currentColor, canvas, width);
